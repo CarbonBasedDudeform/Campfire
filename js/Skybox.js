c6dd3896c2edaced711357526bbfe7dd30d8a2f9
Dom Musgrave <imdomdude@gmail.com>
2014-05-07 15:41:45 +0100

    Changed the skybox
    Changed the skybox texture and updated the code as before I was
    rendering the skybox below the ground where it cannot be seen.

--- js/Skybox.js
+++ js/Skybox.js
@@ -5,24 +5,27 @@ function Skybox(scene)
 	var gmaterial = new THREE.MeshBasicMaterial( {map: skyTexture, side: THREE.DoubleSide} );
 	var planezy = new THREE.Mesh( geometry, gmaterial );
 	planezy.position.z = -500000;
+	planezy.position.y = 500000;
 	scene.add( planezy );
 	var planezy2 = new THREE.Mesh( geometry, gmaterial );
-	planezy2.position.z = 500000;
+	planezy2.position.z = 500000;	
+	planezy2.position.y = 500000;
 	scene.add( planezy2 );
 	var planexy = new THREE.Mesh( geometry, gmaterial );
 	planexy.position.x = -500000;
+	planexy.position.y = 500000;
 	planexy.rotation.y += Math.PI / 2;
 	scene.add( planexy );
 	var planexy2 = new THREE.Mesh( geometry, gmaterial );
 	planexy2.position.x = 500000;
+	planexy2.position.y = 500000;
 	planexy2.rotation.y += Math.PI / 2;
 	scene.add( planexy2 );
+
+	geometry = new THREE.PlaneGeometry( 1000000, 1000000 );
 	var planetop = new THREE.Mesh( geometry, gmaterial );
-	planetop.position.y = 500000;
+	planetop.position.y = 1000000;
 	planetop.rotation.x += Math.PI / 2;
 	scene.add( planetop );
-	var planebot = new THREE.Mesh( geometry, gmaterial );
-	planebot.position.y = -500000;
-	planebot.rotation.x += Math.PI / 2;
-	scene.add( planebot );
+
 }
