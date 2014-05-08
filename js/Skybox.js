b0b74e006b981186180aa6b99a947712a0689a48
Dom Musgrave <imdomdude@gmail.com>
2014-05-08 17:55:02 +0100

    Lower Requirements Improve Performance
    Lowered the values of some items to make the world smaller to improve
    efficiency. World size was 1000000 which was unnecessarily large
    (probably 90% was just trees in darkness...). Also reduced the skybox
    image from something like 50000x2000 to 1000x600 as it works just as
    well at cuts the file size from 11mb to 120kb. Reducing the world size
    also means reducing the number of trees so no longer processing all of
    them.

--- js/Skybox.js
+++ js/Skybox.js
@@ -1,30 +1,30 @@
 function Skybox(scene)
 {
 	var skyTexture = new THREE.ImageUtils.loadTexture( 'img/skybox.jpg' );
-	var geometry = new THREE.PlaneGeometry( 1000000, 1000000 );
+	var geometry = new THREE.PlaneGeometry( 50000, 50000 );
 	var gmaterial = new THREE.MeshLambertMaterial( {map: skyTexture, side: THREE.DoubleSide} );
 	var planezy = new THREE.Mesh( geometry, gmaterial );
-	planezy.position.z = -500000;
-	planezy.position.y = 500000;
+	planezy.position.z = -25000;
+	planezy.position.y = 25000;
 	scene.add( planezy );
 	var planezy2 = new THREE.Mesh( geometry, gmaterial );
-	planezy2.position.z = 500000;	
-	planezy2.position.y = 500000;
+	planezy2.position.z = 25000;	
+	planezy2.position.y = 25000;
 	scene.add( planezy2 );
 	var planexy = new THREE.Mesh( geometry, gmaterial );
-	planexy.position.x = -500000;
-	planexy.position.y = 500000;
+	planexy.position.x = -25000;
+	planexy.position.y = 25000;
 	planexy.rotation.y += Math.PI / 2;
 	scene.add( planexy );
 	var planexy2 = new THREE.Mesh( geometry, gmaterial );
-	planexy2.position.x = 500000;
-	planexy2.position.y = 500000;
+	planexy2.position.x = 25000;
+	planexy2.position.y = 25000;
 	planexy2.rotation.y += Math.PI / 2;
 	scene.add( planexy2 );
 
-	geometry = new THREE.PlaneGeometry( 1000000, 1000000 );
+	geometry = new THREE.PlaneGeometry( 50000, 50000 );
 	var planetop = new THREE.Mesh( geometry, gmaterial );
-	planetop.position.y = 1000000;
+	planetop.position.y = 25000;
 	planetop.rotation.x += Math.PI / 2;
 	scene.add( planetop );
 
